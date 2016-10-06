212c411c183b2100916602c9488d5ebfcfdb032f
余金良 <yujinliang026@foxmail.com>
2016-10-06 18:08:39 +0800

    trigger 修改

--- src/main/java/other/aakepi/bdfjfaackcpic/trigger/pur/PaymentTrigger.java
+++ src/main/java/other/aakepi/bdfjfaackcpic/trigger/pur/PaymentTrigger.java
@@ -1,9 +1,5 @@
 package other.aakepi.bdfjfaackcpic.trigger.pur;
 
-import other.aakepi.bdfjfaackcpic.api.BaseResponse;
-import other.aakepi.bdfjfaackcpic.http.MincsoftHttpClient;
-import other.aakepi.bdfjfaackcpic.trigger.BaseTrigger;
-import other.aakepi.bdfjfaackcpic.util.Configuration;
 import com.rkhd.platform.sdk.exception.ScriptBusinessException;
 import com.rkhd.platform.sdk.http.RkhdHttpClient;
 import com.rkhd.platform.sdk.http.RkhdHttpData;
@@ -13,7 +9,11 @@ import com.rkhd.platform.sdk.model.DataModel;
 import com.rkhd.platform.sdk.param.ScriptTriggerParam;
 import com.rkhd.platform.sdk.param.ScriptTriggerResult;
 import com.rkhd.platform.sdk.test.tool.TestTriggerTool;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import other.aakepi.bdfjfaackcpic.api.QueryResult;
+import other.aakepi.bdfjfaackcpic.trigger.BaseTrigger;
+import other.aakepi.bdfjfaackcpic.util.Configuration;
 
 import java.io.IOException;
 import java.util.List;
@@ -32,12 +32,14 @@ public class PaymentTrigger extends BaseTrigger {
 
         if (list != null && list.size() > 0) {
             DataModel dataModel = list.get(0);
-            int stage = Integer.parseInt(dataModel.getAttribute("stage") + "");
+            int stage = dataModel.getAttribute("stage")!=null&&!"".equals(dataModel.getAttribute("stage"))?Integer.parseInt(dataModel.getAttribute("stage")+""):0;
             String contractId = (String) dataModel.getAttribute("contractId");
-            double amount = Double.valueOf(dataModel.getAttribute("amount") + "");
+            double amount = dataModel.getAttribute("amount")!=null&&!"".equals(dataModel.getAttribute("amount"))?Double.valueOf(dataModel.getAttribute("amount") + ""):0.0;
 
             //1 获取本期付款计划
             String sql = "select id,amount from paymentPlan where stage =" + stage + " and contractId = '" + contractId + "'";
+            sql = "select id,amount from paymentPlan where stage =1";
+            amount = 2000.0;
             RkhdHttpClient rkhdHttpClient = null;
             try {
                 rkhdHttpClient = new RkhdHttpClient();
@@ -51,18 +53,32 @@ public class PaymentTrigger extends BaseTrigger {
                 String planResultJson = rkhdHttpClient.performRequest(rkhdHttpData);
                 System.out.println("planResultJson---------" + planResultJson);
                 JSONObject planResult = JSONObject.fromObject(planResultJson);
+                QueryResult queryResult = (QueryResult)JSONObject.toBean(planResult,QueryResult.class);
 
-                double planAmount = planResult.getDouble("amount");
-                String id = planResult.getString("id");
-                if (Math.abs(amount - planAmount) < 0.01 || amount > planAmount) {//如果实际付款金额>=计划付款金额(考虑尾差),就更新付款计划状态
-                    String json = "{id:"+id+",status:已付款}";
-                    JSONObject params = new JSONObject();
-                    params.put("access_token", "Bearer%20" + getAuthToken());
-                    params.put("json", json);
+                JSONArray array = queryResult.getRecords();
 
-                    //2 更新状态
-                    BaseResponse res = MincsoftHttpClient.post(Configuration.getInstance().getValue("update_entity_data"), params.toString());
+                if (array!=null&&array.size()>0){
+                    JSONObject item = (JSONObject) array.get(0);
+                    double planAmount = item.getDouble("amount");
+                    String id = item.getString("id");
+                    if (Math.abs(amount - planAmount) < 0.01 || amount > planAmount) {//如果实际付款金额>=计划付款金额(考虑尾差),就更新付款计划状态
+                        String json = "{\"id\":"+id+",\"status\":\"已付款\"}";
+//                        JSONObject params = new JSONObject();
+//                        params.put("access_token", "Bearer%20" + getAuthToken());
+//                        params.put("json", json);
+
+                        //2 更新状态
+                        rkhdHttpData = new RkhdHttpData();
+                        rkhdHttpData.setCallString(Configuration.getInstance().getValue("update_entity_data"));
+                        rkhdHttpData.setCall_type("POST");
+                        rkhdHttpData.putFormData("json", json);
+                        String resultStr = rkhdHttpClient.performRequest(rkhdHttpData);
+                        System.out.println("result ----------"+resultStr);
+//                        BaseResponse res = MincsoftHttpClient.post(Configuration.getInstance().getValue("update_entity_data"), params.toString());
+                    }
                 }
+
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -79,8 +95,8 @@ public class PaymentTrigger extends BaseTrigger {
 
     public static void main(String[] args) {
 		TestTriggerTool testTriggerTool = new TestTriggerTool();
-		PaymentTrigger saveMettingTrigger = new PaymentTrigger();
-		testTriggerTool.test("/Users/yujinliang/Documents/workspace/media/src/main/java/scriptTrigger.xml", saveMettingTrigger);
+		PaymentTrigger paymentTrigger = new PaymentTrigger();
+		testTriggerTool.test("/Users/yujinliang/Documents/workspace/media/src/main/java/scriptTrigger.xml", paymentTrigger);
     }
 
 
