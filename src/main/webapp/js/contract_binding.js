db794abf0e8bbad8d9450154d48c5f7a52c5ee06
余金良 <yujinliang026@foxmail.com>
2016-11-12 15:41:11 +0800

    js绑定临时实现

--- src/main/webapp/js/contract_binding.js
+++ src/main/webapp/js/contract_binding.js
@@ -1,6 +1,8 @@
 var grid_saleMediaSpot_menuId = 10418;
-var widget_saleMediaSpot_menuId = 2002;
-var widget_purMediaSpot_menuId = 2002;
+var widget_saleMediaSpot_menuId = 10702;
+var widget_purMediaSpot_menuId = 10703;
+
+
 
 //页面的dom节点和销售易代码准备完毕后触发
 rkclient.on('init:page', function () {
@@ -8,13 +10,15 @@ rkclient.on('init:page', function () {
 });
 
 //1 标准的crm列表页数据加载后触发
-rkclient.on('data:crmGrid', function (gridId, appName) {
-    console.log('crmGrid数据加载完后触发:grid:' + gridId + " app:" + appName);
+rkclient.on('data:crmGrid', function (gridId, entityName) {
+    console.log('crmGrid数据加载完后触发:grid:' + gridId + " app:" + entityName);
+
+    var viewName = $(".js-smartview-tab .view_name").attr("title");
 
     if (gridId == 'div_main_content') {
         var ul = $(".grid_operate ul");
 
-        if (appName == 'contractMediaPaint'){
+        if (entityName == 'contractMediaPaint'||viewName.indexOf('媒体上画') > 0){
             $('<li><a href="javascript:;" target="_blank" class="change" act="paint">批量上画</a></li>').appendTo(ul);
             $("[act=paint]").click(function () {
                 // var access_token = request.getParam("access_token");
@@ -47,25 +51,27 @@ rkclient.on('data:crmGrid', function (gridId, appName) {
 
         }
 
-        if (appName == 'media'){
+        if (entityName == 'media'||viewName.indexOf('媒体库') > 0){
             // a 绘制按钮
-            $('<li><a href="javascript:;" target="_blank" class="change" act="viewMediaSpot">查看排期</a></li>').appendTo(ul);
-            $("#crm_toolbar .grid_tool").prepend('<div class="create-entity-toolbar lfloat prelative"> <a style="" class="create-entity-btn dinline-block lfloat" target="_blank" act="viewMediaSpot"> <i class="dinline-block lfloat"></i>查看排期</a> </div>');
+            $('<li><a href="javascript:;"  class="change" act="viewMediaSpot">查看排期</a></li>').appendTo(ul);
+            $("#crm_toolbar .grid_tool").prepend('<div class="create-entity-toolbar lfloat prelative"> <a style="" class="create-entity-btn dinline-block lfloat" act="viewMediaSpot"> <i class="dinline-block lfloat"></i>查看排期</a> </div>');
 
-            // b 绑定事件
-            // $("[act=viewMediaSpot]").attr("href",'saleContractSpotSearch.html?&id=' + data.id+'&op=view');
+            //b 绑定事件
             $("[act=viewMediaSpot]").click(function () {
                 // var access_token = request.getParam("access_token");
                 // console.log('选择的数据:' + access_token);
-                var selectArray = $('#div_main_content').customerList('getSelectedData');
-                if (selectArray.length == 1) {
-                    var data = selectArray[0];
-                    var menuId = grid_saleMediaSpot_menuId;
-                    var url = "custom_menu.action?menuId=" + menuId + "&id=" + data.id + "&op=view";
-                    window.open(url, '查看排期');
-                } else {
-                    alert("请选择单条合同数据");
-                }
+                var menuId = grid_saleMediaSpot_menuId;
+                var url = "custom_menu.action?menuId=" + menuId;
+                window.location = url;
+                // var selectArray = $('#div_main_content').customerList('getSelectedData');
+                // if (selectArray.length == 1) {
+                //     var data = selectArray[0];
+                //     var menuId = grid_saleMediaSpot_menuId;
+                //     var url = "custom_menu.action?menuId=" + menuId + "&id=" + data.id + "&op=view";
+                //     window.open(url, '查看排期');
+                // } else {
+                //     alert("请选择单条合同数据");
+                // }
             });
         }
     }
@@ -75,7 +81,8 @@ rkclient.on('data:crmGrid', function (gridId, appName) {
 //2 详细页 等待控件类型为xsyCustomizeRefer的数据加载完后触发
 rkclient.on('data:xsyCustomizeRefer', function (id, entityName, entityInfo, widgetInfo, data) {
     // console.log('crmGrid数据加载完后触发:grid:' + id + " appName:" + entityName + " appInfo:" + entityInfo + " widgetInfo:" + widgetInfo);
-    var entityIsTrue = entityName == 'contract' || entityName == 'purchasingContract';
+    var entityId = entityInfo.id;
+    var entityIsTrue = entityName == 'contract' || entityName == 'purchasingContract'||entityId == 100018099;
     var widgetIsTrue = data.name == '采购媒体排期明细' || data.name == '外购媒体排期明细' || data.name == '销售媒体排期明细';
     if (entityIsTrue && widgetIsTrue) {
         // a 绘制按钮
@@ -89,7 +96,7 @@ rkclient.on('data:xsyCustomizeRefer', function (id, entityName, entityInfo, widg
             // console.log('选择的数据:' + access_token);
             var contractId = $(".xsy_su_refer_buttonset .xsy_su_refer_create").attr("businessid");
             var menuId = widget_saleMediaSpot_menuId;
-            if (entityName == 'purchasingContract') {
+            if (entityName == 'purchasingContract'||entityId == 100018099) {
                 menuId = widget_purMediaSpot_menuId;
             }
             var url = "custom_menu.action?menuId=" + menuId + "&id=" + contractId + "&op=edit";
